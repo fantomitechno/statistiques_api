71cf877f76de64a3a5793f3b94a33a59bfd85792
fantomitechno <67735304+fantomitechno@users.noreply.github.com>
2022-05-26 15:30:28 +0200

    test: all testing stuff

--- tests/index.ts
+++ tests/index.ts
@@ -6,6 +6,22 @@ const logs = async (res: Response) => {
 
 const main = async () => {
   await fetch("http://localhost:3000").then(logs);
+
+  await fetch("http://localhost:3000/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id: "test" }),
+  }).then(logs);
+
+  await fetch("http://localhost:3000/stats/test").then(logs);
+
+  await fetch("http://localhost:3000/incremente/test/1", {
+    method: "POST",
+  }).then(logs);
+
+  await fetch("http://localhost:3000/stats/test?choice_id=1").then(logs);
 };
 
 main();
